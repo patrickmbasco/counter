9cc70a8ed0855ac2b56dc803407241348f333a74
Patrick Basco <patrickbasco@Patricks-MacBook-Air.local>
2020-04-19 21:00:47 -0700

    first commit

---
+++ src/main/java/com/pbasco/Counter/ResultController.java
@@ -0,0 +1,21 @@
+package com.pbasco.Counter;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResultController {
+	@RequestMapping("/counter")
+	public String results(HttpSession session) {
+		if (session.getAttribute("count") == null) {
+			session.setAttribute("count", 0);
+		}
+        Integer count = (Integer) session.getAttribute("count");
+		count += 1;
+		session.setAttribute("count", count);
+		return "results.jsp";
+	}
+
+}
